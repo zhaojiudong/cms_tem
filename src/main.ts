ab6e04552b3ee6670a323b3cafd402aeb7c1a152
zhaozhendong <876602326@qq.com>
2024-04-14 22:40:47 +0800

    feat: 初始化守卫，拦截器等基础功能

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
+import { LoginGuard } from '@guard/login.guard';
+import { PermissionGuard } from '@guard/permission.guard';
+import { FormatResponseInterceptor } from '@interceptor/format-response.interceptor';
+import { InvokeRecordInterceptor } from '@interceptor/invoke-record.interceptor';
 import { AppModule } from './app.module';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalGuards(new LoginGuard());
+  app.useGlobalGuards(new PermissionGuard());
+
+  app.useGlobalPipes(new ValidationPipe());    
+
+  app.useGlobalInterceptors(new FormatResponseInterceptor());
+  app.useGlobalInterceptors(new InvokeRecordInterceptor()); 
 
   const configService = app.get(ConfigService);
   await app.listen(configService.get('nest_server_port'));

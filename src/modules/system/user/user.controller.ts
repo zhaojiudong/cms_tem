b688ab6de07b7233c0ac059b98242238c142a2b1
zhaozhendong <876602326@qq.com>
2024-06-23 12:03:08 +0800

    feat: 上传env文件

--- src/modules/system/user/user.controller.ts
+++ src/modules/system/user/user.controller.ts
@@ -1,11 +1,13 @@
 
 import {
+  Body,
   Controller,
   Get,
+  Post,
   Query
 } from '@nestjs/common';
 import { AjaxResult } from 'src/modules/core/class/ajax-result';
-import { ListUserDto } from './dto/user.dto';
+import { CreateUserDto, ListUserDto } from './dto/user.dto';
 import { UserService } from "./user.service";
 
 
@@ -34,5 +36,29 @@ export class UserController {
     return AjaxResult.success(await this.userService.list(user))
   }
 
+
+  /**
+ * 添加用户
+ * @param user 用户信息
+ */
+  @Post('add')
+  // @Log({ title: '用户管理', operType: OperType.INSERT })
+  // @RequirePermissions('system:user:add')
+  async add(@Body() user: CreateUserDto): Promise<AjaxResult> {
+    if (!(await this.userService.checkUserNameUnique(user))) {
+      return AjaxResult.error(`新增用户${user.userName}失败，登录账号已存在`)
+    }
+
+    if (!(await this.userService.checkUserEmailUnique(user))) {
+      return AjaxResult.error(`新增用户${user.userName}失败，邮箱账号已存在`)
+    }
+
+    if (!(await this.userService.checkUserPhoneUnique(user))) {
+      return AjaxResult.error(`新增用户${user.userName}失败，手机号码已存在`)
+    }
+
+    user.createBy = this.securityContext.getUserName()
+    return AjaxResult.success(await this.userService.add(user))
+  }
 
 }
